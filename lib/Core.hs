809b08902905613e383bf1ce774558b57be64669
Adrian Dapprich <adrian.da@outlook.com>
2019-10-13 22:17:10 +0200

    Implement threepenny-gui frontend

--- lib/Core.hs
+++ lib/Core.hs
@@ -3,22 +3,28 @@ module Core where
 type Name = String
 type Initiative = Int
 type Round = Int
+data Allegiance = Friend | Foe deriving Eq
 data Combatant = Combatant { name :: Name
                            , initiative :: Initiative
+                           , allegiance :: Allegiance
                            }
+                 deriving Eq
 
 instance Show Combatant where
-  show c = show $ name c
+  show c = (name c) ++ " (" ++ show (initiative c) ++ ")" 
 
 newtype CombatOrder = CombatOrder [(Int,[Combatant])]
   deriving Show
 newtype Combat = Combat [Combatant]
 
 theHeroes :: [Combatant]
-theHeroes = [ Combatant "Nolleman" 5
-            , Combatant "Hassan" 7
-            , Combatant "Johann" 14
+theHeroes = [ Combatant "Hassan" 7 Friend
+            , Combatant "Nolleman" 9 Friend
+            , Combatant "Johann" 13 Friend
             ]
+
+isFriend :: Combatant -> Bool
+isFriend c = allegiance c == Friend
 
 mkCombat :: [Combatant] -> Combat
 mkCombat heroes = Combat heroes
@@ -35,9 +41,10 @@ a <|> b = b `mod` a == 0
 getCombatOrder :: Combat -> Round -> Int -> CombatOrder
 getCombatOrder (Combat combatants) round l =
   CombatOrder
+  $ take l
   $ filter (\(_,cs) -> length cs > 0)
   $ fmap assignCombatants rounds
-  where rounds = [round..round+l-1]
+  where rounds = [round..]
         assignCombatants :: Int -> (Int,[Combatant])
         assignCombatants i = (i, filter (\c -> initiative c <|> i) combatants)
 
--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,129 @@
 module Main where
 
+{-# LANGUAGE ScopedTypeVariables #-}
+
+import qualified Graphics.UI.Threepenny as UI
+import Graphics.UI.Threepenny.Core
+
+
+import Data.IORef
+import Control.Monad (void, liftM, liftM3)
+import qualified Data.List as L (delete)
+import Data.Bool (bool)
+import Data.List (intersperse)
+
+import Core
+
+combatOrderLength = 10
+  
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = startGUI defaultConfig { jsStatic = Just "." } setup
+
+setup :: Window -> UI ()
+setup window = do
+  UI.addStyleSheet window "main.css"
+  return window # set UI.title "Initiative Counter"
+
+  -- active elements
+  bsubmit <- UI.button # set UI.text "+"
+  tname <- UI.input
+  tinitiative <- UI.input
+  bswitchcombat <- UI.button
+  -- bendcombat <- UI.button # set UI.text "End Combat"
+  bnextround <- UI.button # set UI.text "Next Round"
+  sround <- UI.span
+  
+  -- state
+  -- combatants :: IORef [Combatant]
+  combatants <- liftIO $ newIORef theHeroes
+  -- round :: IORef Int
+  roundRef <- liftIO $ newIORef 1
+  nextRoundRef <- liftIO $ newIORef 1
+  -- combat :: IORef Bool
+  combat <- liftIO $ newIORef False
+  
+  -- functionality
+  let
+    combatantElement :: Combatant -> UI Element
+    combatantElement c = do
+      bdelete <- UI.button # set UI.text "-"
+      on UI.click bdelete $ \_ -> removeCombatant c >> redoLayout
+      
+      row [ UI.span # set UI.text (show c)
+          , element bdelete ]
+      
+    redoLayout :: UI ()
+    redoLayout = void $ do
+      layout <- mkLayout =<< liftIO (readIORef combatants)
+      getBody window # set children [ layout ]
+
+    mkLayout :: [Combatant] -> UI Element
+    mkLayout cs = do
+      let combatantSection = map combatantElement cs
+      
+      inCombat <- liftIO $ readIORef combat
+      set UI.text (bool "Start Combat" "End Combat" inCombat) $ return bswitchcombat
+      
+      round <- liftIO $ readIORef roundRef
+      -- this lifts the functions instead of reading out the values of the IORef but it not really more legible
+      -- combat <- liftIO $ liftM Combat (readIORef combatants)
+      -- CombatOrder combatOrder <- liftIO $ liftM3 getCombatOrder (liftM Combat (readIORef combatants)) (readIORef round) (readIORef combatOrderLengthRef)
+      let combat = Combat cs
+          CombatOrder combatOrder = getCombatOrder combat round combatOrderLength
+          combatOrderSection = if not inCombat then []
+                               else [ UI.hr
+                                    , UI.ul #+ map toCombatElement combatOrder
+                                    ]
+          nextRound = fst $ combatOrder !! 1
+
+      element sround # set UI.text ("Current Round: " ++ (show round))
+      liftIO $ modifyIORef nextRoundRef (const nextRound)
+      
+      column $
+        [ row [ element tname, element tinitiative, element bsubmit ]
+        , UI.hr ]
+        ++ combatantSection ++
+        [ UI.hr
+        , row [ element bswitchcombat, element bnextround, element sround ] ]
+        ++ combatOrderSection
+
+    toCombatElement :: (Int,[Combatant]) -> UI Element
+    toCombatElement (i,cs) = UI.li #+ [ UI.span # set UI.text (show i ++ ": ")
+                                      , UI.span #+ spans ]
+      where spans = map (\c -> UI.span #. allegianceCSS (allegiance c) # set UI.text (name c)) cs
+
+    allegianceCSS :: Allegiance -> String
+    allegianceCSS Friend = "friend"
+    allegianceCSS Foe = "foe"
+
+    addCombatant :: UI ()
+    addCombatant = do
+      n <- get UI.value tname
+      si <- get UI.value tinitiative
+      inCombat <- liftIO $ readIORef combat
+      let i = (read si :: Int)
+          combatant = Combatant { name = n, initiative = i, allegiance = bool Friend Foe inCombat }
+          
+      liftIO $ modifyIORef combatants (combatant:)
+      
+    removeCombatant :: Combatant -> UI ()
+    removeCombatant c = liftIO $ modifyIORef combatants (L.delete c)
+
+    toNextRound :: UI ()
+    toNextRound = do
+      nextRound <- liftIO $ readIORef nextRoundRef
+      liftIO $ modifyIORef roundRef (const nextRound)
+
+    switchCombat :: UI ()
+    switchCombat = do
+      inCombat <- liftIO $ readIORef combat
+      if inCombat
+        then liftIO $ modifyIORef combatants (filter isFriend)
+        else return ()
+      liftIO $ modifyIORef combat not
+
+  on UI.click bsubmit $ \_ -> addCombatant >> redoLayout
+  on UI.click bnextround $ \_ -> toNextRound >> redoLayout
+  on UI.click bswitchcombat $ \_ -> switchCombat >> redoLayout
+  
+  redoLayout
