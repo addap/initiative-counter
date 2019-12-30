30901f30f02a62aaa054fec90e21bc10ef452154
Adrian Dapprich <adrian.da@outlook.com>
2019-12-30 21:56:04 +0100

    Refactor to use a more FRP style

--- src/Main.hs
+++ src/Main.hs
@@ -6,21 +6,49 @@ import qualified Graphics.UI.Threepenny as UI
 import Graphics.UI.Threepenny.Core
 
 
-import Data.IORef
-import Control.Monad (void)
+import Control.Monad (void, liftM, liftM2)
 import qualified Data.List as L (delete)
 import Data.Bool (bool)
 import Data.List (intersperse)
+import Data.Bifunctor
+import Data.Maybe (fromMaybe)
+import Text.Read (readMaybe)
 
 import Core
 
+data Mode = Fight | Normal deriving (Eq, Show)
+
+modeBool :: Mode -> Bool
+modeBool Fight = True
+modeBool Normal = False
+
+switchMode :: Mode -> Mode
+switchMode Fight = Normal
+switchMode Normal = Fight
+
+modeToAllegiance :: Mode -> Allegiance
+modeToAllegiance Fight = Foe
+modeToAllegiance Normal = Friend
+
+modeButton :: Mode -> String
+modeButton Fight = "End Combat"
+modeButton Normal = "Start Combat"
+
+modeDisplay :: Mode -> String
+modeDisplay Fight = "block"
+modeDisplay Normal = "none"
+
 combatOrderLength = 10
+
+-- unionWith that always takes the first event
+infixl 4 <=>
+(<=>) = unionWith const
   
 main :: IO ()
 main = startGUI defaultConfig { jsStatic = Just "static", jsAddr = Nothing, jsPort = Nothing } setup
 
 setup :: Window -> UI ()
-setup window = do
+setup window = void $ do
   UI.addStyleSheet window "bootstrap.min.css"
   UI.addStyleSheet window "main.css"
 
@@ -28,122 +56,116 @@ setup window = do
   meta <- UI.meta # set UI.content "width=device-width, initial-scale=1" # set UI.name "viewport"
   getHead window #+ [ element meta ]
   
-  return window # set UI.title "Initiative Counter"
+  window # set' UI.title "Initiative Counter"
 
-  -- active elements
+  -- display property to hide elements
+  let display :: Attr Element String
+      display = fromObjectProperty "style.display"
+
+    -- active elements
   bsubmit <- UI.button # set UI.text "+"
   tname <- UI.input
   tinitiative <- UI.input
   bswitchcombat <- UI.button
-  -- bendcombat <- UI.button # set UI.text "End Combat"
+  bendcombat <- UI.button # set UI.text "End Combat"
   bnextround <- UI.button # set UI.text "Next Round"
   sround <- UI.span
+  dCombatOrderSection <- UI.div
+  dCombatantSection <- UI.div
+
+  (deleteEvt, deleteHdl) <- liftIO $ (newEvent :: IO (Event (Combatant, Element), Handler (Combatant, Element)))
+  liftIO $ register deleteEvt (\(n, _) -> putStrLn (show n))
+
+    -- behaviors & events
+  let eChangeMode = UI.click bswitchcombat
+      eNextRound = UI.click bnextround
+  -- make behaviors out of name and initiative
+  bName <- stepper "" $ UI.valueChange tname
+  bInitiative <- stepper 0 $ fromMaybe 1 . (readMaybe :: String -> Maybe Int) <$> UI.valueChange tinitiative
+  -- behavior that tracks if we are in fight or normal mode
+  bMode <- accumB Normal $ (const switchMode) <$> eChangeMode
+  -- filter the cases where bMode == Fight since bMode is only changed slightly after the eChangeMode event happens
+  let eEndCombat = filterE (==Fight) (bMode <@ eChangeMode)
+      eResetRound = const 1 <$> eEndCombat
+      eRemoveFoes = const (filter (\c -> allegiance c == Friend)) <$> eEndCombat
   
-  -- state
-  -- combatants :: IORef [Combatant]
-  combatants <- liftIO $ newIORef theHeroes
-  -- round :: IORef Int
-  roundRef <- liftIO $ newIORef 1
-  nextRoundRef <- liftIO $ newIORef 1
-  -- combat :: IORef Bool
-  combat <- liftIO $ newIORef False
+  -- behavior that contains the current "new combatant" i.e. depending on what the name and initiative fields contain and in which mode we are now
+  let bNewCombatant = Combatant <$> bName <*> bInitiative <*> (modeToAllegiance <$> bMode)
+      -- event that contains the new combatant by attaching the current state of the behavior to each click on bsubmit
+      eNewCombatant = bNewCombatant <@ UI.click bsubmit
+      -- map cons over the event to turn it into a function that adds a new combatant
+      eAddCombatant = (:) <$> eNewCombatant
+      eDeleteCombatant = L.delete . fst <$> deleteEvt
+      eChangeCombatants = eAddCombatant <=> eDeleteCombatant <=> eRemoveFoes
+  -- behavior that contains the current combatants. changed by eChangeCombatants
+  bCombatants <- accumB [] eChangeCombatants
+
+  let eAddCombatantElement = combatantElement deleteHdl <$> eNewCombatant
+  -- register to event to add ui elements
+  liftIO $ register eAddCombatantElement $ \e -> runUI window (element dCombatantSection #+ [e]) >> return ()
+  -- delete combatants when their '-' button is pressed
+  liftIO $ register deleteEvt $ runUI window . delete . snd
+  -- instead of registering both functions and using runUI I could also make a behavior for both events and use the onChange event handler from that to do the same thing.
+
+  (eStepRound, hStepRound) <- liftIO $ (newEvent :: IO (Event Round, Handler Round))
+  bRound <- stepper 1 $ unionWith const eStepRound eResetRound
+  -- bahvior for the current combat order
+  -- let bCombatOrder :: UI (Behavior CombatOrder)
+  let bCombatOrder = getCombatOrderFrom <$> bRound <*> bCombatants
+      -- bRound :: UI (Behavior Round)
+      -- bNextRound :: UI (Behavior Round)
+      bNextRound = fmap (\(CombatOrder co) -> fst $ co !! 1) bCombatOrder
+  -- pretty ugly but it allows me to notify the stepper of bRound that it should step to the next value which is currently in bRound
+  -- FIXME is there a better way to do recursive Behaviors?
+  liftIO $ register eNextRound $ const $ runUI window (currentValue bNextRound) >>= hStepRound
+
+  onChanges bCombatOrder (\(CombatOrder co) -> do
+                             elm <- UI.ul #+ map toCombatElement (take 10 co)
+                             element dCombatOrderSection # set children [ elm ])
+
+  element sround # sink UI.text (("Current Round: " ++) . show <$> bRound)
+  element bswitchcombat # sink UI.text (modeButton <$> bMode)
+  element dCombatOrderSection # sink display (modeDisplay <$> bMode)
   
-  -- functionality
-  let
-    combatantElement :: Combatant -> UI Element
-    combatantElement c = do
-      bdelete <- UI.button # set UI.text "-"
-      on UI.click bdelete $ \_ -> removeCombatant c >> redoLayout
-      
-      UI.div # brow #+ [ UI.span # bcol 10 # set UI.text (show c)
-                         , element bdelete # bcol 2 ]
-      
-    redoLayout :: UI ()
-    redoLayout = void $ do
-      layout <- mkLayout =<< liftIO (readIORef combatants)
-      getBody window # set children [ layout ]
+  let content =
+        [ UI.div # brow #+ [ element tname # bcol 5, element tinitiative # bcol 5, element bsubmit # bcol 2 ]
+        , UI.hr ]
+        ++ [ element dCombatantSection ] ++
+        [ UI.hr
+        , UI.div # brow #+ [ element bswitchcombat # bcol 4, element bnextround # bcol 4, element sround # bcol 4 ] ]
+        ++ [ element dCombatOrderSection ]
+  
+      layout =  UI.div #. "container-fluid" #+
+                [ UI.div # brow #+
+                  [ UI.div # bcol 12 #+
+                    [ UI.div #. "jumbotron" #+ content ]
+                  ]
+                ]
 
-    mkLayout :: [Combatant] -> UI Element
-    mkLayout cs = do
-      let combatantSection = map combatantElement cs
-      
-      inCombat <- liftIO $ readIORef combat
-      set UI.text (bool "Start Combat" "End Combat" inCombat) $ return bswitchcombat
-      
-      round <- liftIO $ readIORef roundRef
-      -- this lifts the functions instead of reading out the values of the IORef but it not really more legible
-      -- combat <- liftIO $ liftM Combat (readIORef combatants)
-      -- CombatOrder combatOrder <- liftIO $ liftM3 getCombatOrder (liftM Combat (readIORef combatants)) (readIORef round) (readIORef combatOrderLengthRef)
-      let combat = Combat cs
-          CombatOrder combatOrder = getCombatOrder combat round combatOrderLength
-          combatOrderSection = if not inCombat then []
-                               else [ UI.hr
-                                    , UI.ul #+ map toCombatElement combatOrder
-                                    ]
-          nextRound = fst $ combatOrder !! 1
+  getBody window #+ [ layout ]
 
-      element sround # set UI.text ("Current Round: " ++ (show round))
-      liftIO $ modifyIORef nextRoundRef (const nextRound)
 
-      let content =
-            [ UI.div # brow #+ [ element tname # bcol 5, element tinitiative # bcol 5, element bsubmit # bcol 2 ]
-            , UI.hr ]
-            ++ combatantSection ++
-            [ UI.hr
-            , UI.div # brow #+ [ element bswitchcombat # bcol 4, element bnextround # bcol 4, element sround # bcol 4 ] ]
-            ++ combatOrderSection
-                              
-      UI.div #. "container-fluid" #+
-        [ UI.div # brow #+
-          [ UI.div # bcol 12 #+
-            [ UI.div #. "jumbotron" #+ content ]
-          ]
-        ]
 
-    toCombatElement :: (Int,[Combatant]) -> UI Element
-    toCombatElement (i,cs) = UI.li #+ [ UI.span # set UI.text (show i ++ ": ")
+toCombatElement :: (Int,[Combatant]) -> UI Element
+toCombatElement (i,cs) = UI.li #+ [ UI.span # set UI.text (show i ++ ": ")
                                       , UI.span #+ spans ]
-      where spans = map (\c -> UI.span #. allegianceCSS (allegiance c) # set UI.text (name c)) cs
+  where spans = map (\c -> UI.span #. allegianceCSS (allegiance c) # set UI.text (name c)) cs
 
-    allegianceCSS :: Allegiance -> String
-    allegianceCSS Friend = "friend"
-    allegianceCSS Foe = "foe"
-
-    addCombatant :: UI ()
-    addCombatant = do
-      n <- get UI.value tname
-      si <- get UI.value tinitiative
-      inCombat <- liftIO $ readIORef combat
-      let i = (read si :: Int)
-          combatant = Combatant { name = n, initiative = i, allegiance = bool Friend Foe inCombat }
-          
-      liftIO $ modifyIORef combatants (combatant:)
-      
-    removeCombatant :: Combatant -> UI ()
-    removeCombatant c = liftIO $ modifyIORef combatants (L.delete c)
-
-    toNextRound :: UI ()
-    toNextRound = do
-      nextRound <- liftIO $ readIORef nextRoundRef
-      liftIO $ modifyIORef roundRef (const nextRound)
-
-    switchCombat :: UI ()
-    switchCombat = do
-      inCombat <- liftIO $ readIORef combat
-      if inCombat
-        then liftIO $ modifyIORef combatants (filter isFriend)
-        else return ()
-      liftIO $ modifyIORef combat not
-
-  on UI.click bsubmit $ \_ -> addCombatant >> redoLayout
-  on UI.click bnextround $ \_ -> toNextRound >> redoLayout
-  on UI.click bswitchcombat $ \_ -> switchCombat >> redoLayout
-  
-  redoLayout
+allegianceCSS :: Allegiance -> String
+allegianceCSS Friend = "friend"
+allegianceCSS Foe = "foe"
 
 
+combatantElement :: Handler (Combatant, Element) -> Combatant -> UI Element
+combatantElement hdl c = do
+  bdelete <- UI.button # set UI.text "-"
+  elm <- UI.div # brow #+ [ UI.span # bcol 10 # set UI.text (show c)
+                             , element bdelete # bcol 2 ]
+  liftIO $ register (UI.click bdelete) $ \() -> hdl (c, elm)
+  return elm
 
--- bootsrap functions
+
+-- bootstrap functions
 brow :: UI Element -> UI Element
 brow e = e #. "row"
 
